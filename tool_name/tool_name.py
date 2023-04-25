20f0a9519fe2a3b6aea50c5f5925f7b5adb3d7c8
bhanuduggal <69357690+bhanuduggal@users.noreply.github.com>
2023-04-25 14:07:05 +0530

    update

--- tool_name/tool_name.py
+++ tool_name/tool_name.py
@@ -1,9 +1,9 @@
 import requests
 
-def sample(event, context):
+def lambda_handler(event, context):
     url = 'https://data.nationalgrideso.com/backend/datastore/dump/6fd8e042-be27-4c67-ad59-5acdd2a7b0fd'
     response = requests.get(url)
     print('Response returned by API: ', response.status_code)
 
 if __name__ == "__main__":
-    sample(None, None)
+    lambda_handler(None, None)
