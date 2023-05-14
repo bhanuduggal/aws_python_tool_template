57798d1e722f01d46b1dfe9718390632ed795bfc
bhanuduggal <69357690+bhanuduggal@users.noreply.github.com>
2023-05-14 15:49:36 +0100

    parameter store variables and schedule

--- tool_name/main.py
+++ tool_name/main.py
@@ -1,9 +1,11 @@
+import os
 from env_loader import load_env_variables
 
 def lambda_handler(event, context):
     parameters = load_env_variables()
     REGION = parameters['REGION']
-    print(REGION)
+    environment = 'Local' if os.getenv('ENVIRONMENT') == None else os.getenv('ENVIRONMENT')
+    print(f'{environment} - Lambda is working successfully in {REGION}.')
 
 
 if __name__=="__main__":
